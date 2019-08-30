019870899358f177718f07d40789e09b7f138a10
Aj.Mor.01 <aj.mor.01@gmail.com>
2019-08-30 19:23:18 +0200

    add browser-sync to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,26 @@
-function defaultTask(cb) {
-    // place code for your default task here
-    cb();
-  }
-  
-  exports.default = defaultTask
+const gulp = require('gulp');
+const browserSync = require('browser-sync')
+const server = browserSync.create();
+
+function reload(done) {
+    server.reload();
+    done();
+}
+
+function serve(done) {
+    server.init({
+        server: {
+            baseDir: './src'
+        }
+    });
+    done();
+}
+
+function watchFiles() {
+    gulp.watch(['./src/*.html', './src/css/*.css', './src/js/*.js'],
+    reload);
+}
+
+const watch = gulp.parallel(watchFiles, serve)
+
+exports.default = watch;
